4aa005fea1c5b2ccdf93e9d832baeebf493f5a92
Luong Nguyen Thanh <ph.ntluong95@gmail.com>
2023-11-20 17:08:44 +0100

    reconfigure the UI

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,13 +1,25 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
-
 import github from "../../assets/github.svg";
-
 import styles from "./Layout.module.css";
-
-
+import { useState, useEffect } from "react";
 import { LoginButton } from "../../components/LoginButton";
 
 const Layout = () => {
+    const [contributors, setContributors] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/repos/tuananhdao/chat.duhocsinh.api/contributors')
+        .then(response => response.json())
+        .then(data => {
+            const contribList = data.map(contributor => ({
+                login: contributor.login,
+                avatar_url: contributor.avatar_url,
+                html_url: contributor.html_url,
+            }));
+            setContributors(contribList);
+        });
+    }, []);
+
     return (
         <div className={styles.layout}>
             <header className={styles.header} role={"banner"}>
@@ -18,17 +30,7 @@ const Layout = () => {
                     <nav>
                         <ul className={styles.headerNavList}>
                             <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://github.com/swedenmentor/swedenmentor.github.io" target={"_blank"} title="Github repository link">
+                                <a href="https://github.com/swedenmentor/API" target={"_blank"} title="Github repository link">
                                     <img
                                         src={github}
                                         alt="Github logo"
@@ -41,15 +43,34 @@ const Layout = () => {
                             </li>
                         </ul>
                     </nav>
-                    {
-                        /* <h4 className={styles.headerRightText}>An information hub for international residents living in Sweden</h4> */
-                    }
                 </div>
             </header>
+            {
+            /*
+            <footer className={styles.footer} role={"banner"}>
+                <ul className={styles.footerNavList}>
+                    <li className={styles.footerLabel}>Developers:</li>
+                    {contributors.map((contributor, index) => (
+                    <li key={index}>
+                        <a href={contributor.html_url} target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={contributor.avatar_url}
+                                alt={contributor.login}
+                                width="20"
+                                height="20"
+                                style={{display: "inline", marginTop: "-3px", borderRadius: "50%"}}
+                            />
+                            {contributor.login}
+                        </a>
+                    </li>
+                    ))}
 
+                </ul>
+            </footer>
+            */
+            }
             <Outlet />
         </div>
     );
 };
-
 export default Layout;
